00a2cb261b5f2334c29ca412bafc39d874296b0a
Divyansh Agarwal <dagarwal058@gmail.com>
2023-02-17 23:19:06 +0530

    placeholder

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {IoMdMic} from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
@@ -6,11 +6,22 @@ import './SearchBox.css';
 
 const SearchBox = (props) => {
 
+  const [input, setInput] = useState();
+
+  useEffect(() => {
+    if(props.isSearch)
+    {
+      let input = localStorage.getItem('search');
+      setInput(input);
+    }
+  },[props]);
+ 
+
   let navigate = useNavigate();
   return (
    <div className="search-box">
      <AiOutlineSearch className='search-icon'/>
-     <input type="text" placeholder='Search Google or Type a Url' onKeyDown={e => 
+     <input type="search" placeholder='Search Google or Type a Url' value={input} onInput={e => setInput(e.target.value)} onKeyDown={e => 
      {if(e.key=="Enter")
     {
       if(props.isSearch)
